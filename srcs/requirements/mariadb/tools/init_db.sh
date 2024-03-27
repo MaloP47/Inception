955590e1973ef4a668a1653c0e0c2b0c7e6e0078
Mallory PEULET <mpeulet@gmail.com>
2024-03-27 16:56:01 +0100

    mdb is almost running, need to figure out command line et rework a bit

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,13 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-service mysql start;
+MDB_DB="inception-db"
+MDB_USER="inception-db-user"
+MDB_USER_PASSWORD="dbuserpass123"
+MDB_ROOT_PASSWORD="dbrootpass123"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysqld_safe &
+
+until mysqladmin ping; do
+    echo "Waiting for mariadb to be ready..."
+    sleep 2
+done
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DB}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${MDB_USER}\`@'localhost' IDENTIFIED BY '${MDB_USER_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DB}\`.* TO \`${MDB_USER}\`@'%' IDENTIFIED BY '${MDB_USER_PASSWORD}';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASSWORD}';"
+
+mysql -e "DROP DATABASE IF EXISTS test;"
+
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin shutdown
 
-exec mysqld_safe
+exec mysqld_safe;
