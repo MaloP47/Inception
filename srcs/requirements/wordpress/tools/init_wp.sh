7f50b0ccb8b1c0dd9519ed4728271e083214c1bb
malo <mpeulet@gmail.com>
2024-05-02 14:51:01 +0200

    Little save

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -20,28 +20,26 @@ WP_AUTHOR_MAIL="malo@inception.fr"
 if [ ! -f wp-config.php ]; then
 
     # Update WP-CLI to the latest version
-    wp cli update --yes --allow-root
 
     # Download WordPress core files
-    wp core download --allow-root --version=6.5
 
     # Create the wp-config.php file
-    wp config create --dbname="${MDB_NAME}" --dbuser="${MDB_ADM}" --dbpass="${MDB_PASS}" --dbhost="mariadb:3306" --allow-root
+    wp config create --dbname="${MDB_NAME}" --dbuser="${MDB_ADM}" --dbpass="${MDB_PASS}" --dbhost=mariadb:3306 --allow-root;
 
     # Install WordPress
-    wp core install --url="${MYURL}" --title="${MYSITE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_ADM_MAIL}" --allow-root
+    wp core install --url="${MYURL}" --title="${MYSITE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_ADM_MAIL}" --allow-root;
 
 	sleep 5
     # Create an additional WordPress author user
-    wp user create "${WP_AUTHOR}" "${WP_AUTHOR_MAIL}" --user_pass="${WP_AUTHOR_PASS}" --role=author --allow-root
+    wp user create "${WP_AUTHOR}" "${WP_AUTHOR_MAIL}" --user_pass="${WP_AUTHOR_PASS}" --role=author --allow-root;
 
     # Install and activate a theme, e.g., twentytwentyfour
-    wp theme install twentytwentyfour --activate --allow-root
+    wp theme install twentytwentyfour --activate --allow-root;
 
     # Optional: Display the status of the installed theme
-    wp theme status twentytwentyfour --allow-root
+    wp theme status twentytwentyfour --allow-root;
 
 fi
 
 # Execute the PHP FastCGI Process Manager
-exec php-fpm81 -F
+exec php-fpm81 -F;
