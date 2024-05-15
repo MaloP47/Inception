91543f5b9501887f0b3a33895ad322f5795fb5d1
malo <mpeulet@gmail.com>
2024-05-15 15:50:33 +0200

    solved worpress

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,67 +1,20 @@
-#!/bin/sh
-
-# # Create the wp-config.php file
-# wp config create --dbname=${MDB_DB} \
-#                     --dbuser=${MDB_ADMIN} \
-#                     --dbpass=${MDB_ADMIN_PASS} \
-#                     --dbhost=mariadb:3306
-
-# # Install WordPress
-# wp core install --url=${MYURL} \
-#                 --title=${MYSITE} \
-#                 --admin_user=${WP_ADMIN} \
-#                 --admin_password=${WP_ADMPASS} \
-#                 --admin_email=${WP_ADM_MAIL};
-
-# sleep 5;
-
-# # Create an additional WordPress author user
-# wp user create ${WP_AUTHOR} ${WP_AUTHOR_MAIL} \
-#                 --user_pass=${WP_AUTHOR_PASS} \
-#                 --role=author;
-
-# # Install and activate a theme, e.g., twentytwentyfour
-# wp theme install twentytwentyfour --activate;
-
-# # Optional: Display the status of the installed theme
-# wp theme status twentytwentyfour;
-
-
-
-# # Execute the PHP FastCGI Process Manager
-# /usr/sbin/php-fpm81 -F
-
 #!/bin/bash
 
-# creating user www-data
-adduser -D www-data -G www-data
-
-# testing if wordpress is already installed
 if [ -f /var/www/html/wp-config.php ]; then
-	echo "Worpress already downloaded"
-else
-	# creating html folder for wordpress
-	if [ ! -d /var/www/html ]; then
-		mkdir /var/www/html
-	fi
-	chown -R www-data:www-data /var/www/html
-	cd /var/www/html
-	echo "Downloading wordpress..."
-	curl https://fr.wordpress.org/wordpress-6.4.3-fr_FR.tar.gz -o wp.tar.gz
-	# unziping wordpress
-	tar xfz wp.tar.gz
-	rm -rf wp.tar.gz
-	mv wordpress/* .
-	sleep 5
-	# configuring wordpress with the cli
-	wp-cli config create --allow-root --dbname=$MDB_DB --dbuser=$MDB_ADMIN \
+	:
+else 
+		curl https://fr.wordpress.org/wordpress-6.5.3-fr_FR.tar.gz -o wp.tar.gz
+		tar xfz wp.tar.gz
+		rm -rf wp.tar.gz
+		mv wordpress/* .
+		sleep 5
+		wp-cli config create --allow-root --dbname=$MDB_DB --dbuser=$MDB_ADMIN \
 		--dbpass=$MDB_ADMIN_PASS --dbhost=mariadb:3306
-	wp-cli core install --url=$MYURL --title=$MYSITE --admin_user=$WP_ADMIN \
+		wp-cli core install --url=$MYURL --title=$MYSITE --admin_user=$WP_ADMIN \
 		--admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADM_MAIL
-	wp-cli user create $WP_AUTHOR $WP_AUTHOR_MAIL --role=author --user_pass=$WP_AUTHOR_PASS
+		wp-cli user create $WP_AUTHOR $WP_AUTHOR_MAIL --role=author --user_pass=$WP_AUTHOR_PASS
 
-    chown -R www-data:www-data /var/www/html
+		chown -R wp-data:wp-data /var/www/html
 fi
 
-# running php
 /usr/sbin/php-fpm81 -F
